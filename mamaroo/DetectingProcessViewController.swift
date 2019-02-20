f8677895a8056bf578f7c13ea3c7b63f8de42452
Кирилл Штеффен <kirillshteffen@gmail.com>
2019-02-20 15:47:39 +0600

    stack overflow solution try

--- mamaroo/DetectingProcessViewController.swift
+++ mamaroo/DetectingProcessViewController.swift
@@ -9,16 +9,27 @@
 import UIKit
 import CoreMotion
 import NVActivityIndicatorView
+import ARKit
 
 class DetectingProcessViewController: UIViewController {
     
-    var motion = CMMotionManager()
+    let motion = CMMotionManager()
+    var motionData: [(Double,Double,Double)] = []
+    let deviceMotion = CMDeviceMotion()
+    let motionFrameReference: CMAttitudeReferenceFrame = CMAttitudeReferenceFrame.xArbitraryCorrectedZVertical
+    let kLowPassFilteringFactor: Double = 0.1
+    let noiseReduction: Double = 0
+    var previousLowPassFilteredAccelerationX: Double = 0
+    var previousLowPassFilteredAccelerationY: Double = 0
+    var previousLowPassFilteredAccelerationZ: Double = 0
+    
+    var points: [(Double,Double)] = []
     
     @IBOutlet weak var activityIndicator: NVActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         
     }
     
@@ -26,25 +37,65 @@ class DetectingProcessViewController: UIViewController {
         super.viewDidAppear(true)
         activityIndicator.startAnimating()
         myDeviceMotion()
+        //myAccelerometer()
     }
+    
 
-    func myDeviceMotion() {
-        print("Start DeviceMotion")
-        motion.deviceMotionUpdateInterval  = 0.01
-        motion.startDeviceMotionUpdates(to: OperationQueue.current!) {
+    
+    func myAccelerometer() {
+        print("Start Accelerometer")
+        motion.accelerometerUpdateInterval = 1/100
+
+        motion.startAccelerometerUpdates(to: OperationQueue.current!) {
             (data, error) in
-            //print(data as Any)
+            
             if let trueData =  data {
-                
-                self.view.reloadInputViews()
-                //self.xDevi!.text = String(format: "x (pitch): %.2f", trueData.attitude.pitch)
-                
-                //self.yDevi!.text = String(format: "y (pitch): %.2f", trueData.attitude.roll)
-                //self.zDevi!.text = String(format: "z (pitch): %.2f", trueData.attitude.yaw)
-                print("\(String(format: "%.2f", trueData.attitude.roll)),\(String(format: "%.2f", trueData.attitude.yaw)),\(String(format: "%.2f", trueData.attitude.pitch))")
+                //print("\(String(format: "%.2f", trueData.acceleration.x)),\(String(format: "%.2f", trueData.acceleration.y)),\(String(format: "%.2f", trueData.acceleration.z))")
             }
         }
         return
+    }
+    
+
+    func myDeviceMotion() {
+        print("Start DeviceMotion")
+        motion.deviceMotionUpdateInterval  = 1/100
+        //motion.deviceMotion?.attitude.
+        //deviceMotion.userAcceleration.
+        //motion.attitudeReferenceFrame
+        motion.startDeviceMotionUpdates(to: OperationQueue.current!) {
+            (data, error) in
+            
+            if let trueData =  data {
+                let userAcceleration = trueData.attitude
+                var lowpassFilterAcceleration = CMAcceleration()
+                
+                lowpassFilterAcceleration.x = (userAcceleration.pitch * self.kLowPassFilteringFactor) + (self.previousLowPassFilteredAccelerationX * (1.0 - self.kLowPassFilteringFactor))
+                lowpassFilterAcceleration.y = (userAcceleration.roll * self.kLowPassFilteringFactor) + (self.previousLowPassFilteredAccelerationX * (1.0 - self.kLowPassFilteringFactor))
+                lowpassFilterAcceleration.z = (userAcceleration.yaw * self.kLowPassFilteringFactor) + (self.previousLowPassFilteredAccelerationX * (1.0 - self.kLowPassFilteringFactor))
+                //if (lowpassFilterAcceleration.x > self.noiseReduction || lowpassFilterAcceleration.y > self.noiseReduction) {
+                    //self.points.append((lowpassFilterAcceleration.x,lowpassFilterAcceleration.y))
+                    print("\(String(format: "%.2f", lowpassFilterAcceleration.x)),\(String(format: "%.2f", lowpassFilterAcceleration.y))")
+               
+                    self.previousLowPassFilteredAccelerationX = lowpassFilterAcceleration.x
+                    self.previousLowPassFilteredAccelerationY = lowpassFilterAcceleration.y
+                    self.previousLowPassFilteredAccelerationZ = lowpassFilterAcceleration.z
+                //}
+//                //pitch)
+//
+//                //roll)
+//                //yaw)
+                //print("\(String(format: "%.2f", trueData.attitude.roll)),\(String(format: "%.2f", trueData.attitude.yaw)),\(String(format: "%.2f", trueData.attitude.pitch))")
+//                let vectorCalculator = DisplacementVectorCalculator(deviceMotion: self.motion.deviceMotion!)
+//                let vector = vectorCalculator.getDisplacementVector(deviceMotion: self.motion.deviceMotion!, timeInterval: 10)
+//                print("\(String(format: "%.2f", vector.x)),\(String(format: "%.2f", vector.y)),\(String(format: "%.2f", vector.z))")
+            }
+        }
+        return
+    }
+   
+    func patternCompare(motionData: [(Double,Double,Double)]) {
+        
     }
     
 }
